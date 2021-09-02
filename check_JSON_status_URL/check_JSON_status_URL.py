82e54fcb49465dca1edd1eef213caf6317f9b8fb
Stefan Wuensch <stefan_wuensch@harvard.edu>
2021-09-02 00:16:12 -0400

    Update copyright date

--- check_JSON_status_URL/check_JSON_status_URL.py
+++ check_JSON_status_URL/check_JSON_status_URL.py
@@ -67,7 +67,7 @@
 # 
 # The MIT License (MIT)
 # 
-# Copyright (c) 2016 Stefan Wuensch
+# Copyright (c) 2021 Stefan Wuensch
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
